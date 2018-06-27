333714b7985e5abe91ddec5cba24adde8fd33dab
gauravyadav01 <gauravyadav01@gmail.com>
2018-06-27 00:02:29 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures my_consul'
 long_description 'Installs/Configures my_consul'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'consul'
